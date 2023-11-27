6c3a128555e470dd15daefd0ae3c3afab9c588a8
bella.tassone <itassone@wesleyan.edu>
2023-11-26 19:50:16 -0500

    condensing csv export into single file #1

--- 02_asr.py
+++ 02_asr.py
@@ -1,6 +1,7 @@
 # for downloading data to google storage bucket as a csv
 # https://cloud.google.com/bigquery/docs/samples/bigquery-extract-table
 
+import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
@@ -33,3 +34,26 @@ print(
 
 # To copy files from storage bucket to local (current directory) use command:
 # gsutil cp gs://asr_demo/results/*.csv ./Results/
+
+# You may alternatively make a query to order columns correctly and export as csv directly from bigquery table
+
+query = """
+    SELECT filename, google_asr_text, stt_confidence FROM `wmp-sandbox.asr_demo.asr_test`
+"""
+query_job = client.query(query)  # Make an API request.
+
+vids = []
+transcripts = []
+confs = []
+
+for row in query_job:
+    vids.append(row["filename"])
+    transcripts.append(row["google_asr_text"])
+    confs.append(row["stt_confidence"])
+
+df = pd.DataFrame()
+df['filename'] = vids
+df['google_asr_text'] = transcripts
+df['stt_confidence'] = confs
+
+df.to_csv('./Results/asr_results_ordered.csv', index=False, encoding="utf-8")
